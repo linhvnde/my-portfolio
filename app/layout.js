6cd91d4656c442d7c22d5c0417316ba2d073e1e6
linhvnde <linh.nh.tyty@gmail.com>
2023-11-03 21:36:20 +0100

    Feat: Nav w content

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+  import './globals.css'
+  import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+  const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Linh Nguyen Hoang Portfolio',
-  description:
-    'Linh Nguyen Hoang is a versatile Full Stack Web Developer bringing over six years of hands-on experience in the e-commerce and fintech sectors. An agile, fast learner and tech enthusiast with a deep understanding of product marketing and a proven ability to adapt in fast-paced, dynamic, multinational environments. Committed to utilizing my proficiency to further polish my skills, while contributing significantly to team success. Actively seeking a challenging web development role in an organization that fosters continuous learning and collaboration, and is dedicated to creating innovative yet user-intuitive products.',
-};
+  export const metadata = {
+    title: 'Linh Nguyen Hoang Portfolio',
+    description:
+      'Linh Nguyen Hoang is a versatile Full Stack Web Developer bringing over six years of hands-on experience in the e-commerce and fintech sectors. An agile, fast learner and tech enthusiast with a deep understanding of product marketing and a proven ability to adapt in fast-paced, dynamic, multinational environments. Committed to utilizing my proficiency to further polish my skills, while contributing significantly to team success. Actively seeking a challenging web development role in an organization that fosters continuous learning and collaboration, and is dedicated to creating innovative yet user-intuitive products.',
+  };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body
+          className={`${inter.className} h-[5000px] bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        >
+          <div className="bg-[#fbf0e2] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+          <div className="bg-[#d7fbed] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+          {children}
+        </body>
+      </html>
+    );
+  }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,22 @@
-"use client"
+"use client";
+
+
 import React from 'react'
-import { SocialIcon } from 'react-social-icons'
+
 import { motion } from 'framer-motion'
+import { SocialIcon } from 'react-social-icons'
+
+
+import { links } from '../lib/data';
+
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center'>
-      <motion.div className='flex flex-row items-center'
+    <header className='z-[999] relative top-0 p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center'
+    // sticky 
+    >
+      <motion.div className='flex flex-row mt-1.5 items-center header-1'
         initial={{ x: -500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{
@@ -18,16 +28,37 @@ export default function Header() {
         <SocialIcon url="https://www.linkedin.com/in/linhnhvn-fullstackdev/" fgColor='darkGreen' bgColor='transparent'/>
       </motion.div>
       <motion.div
+        className='flex flex-row mt-1.5 items-center cursor-pointer header-1'
         initial={{ x: 500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{
           duration: 1.5
         }}        
         >
-        {/* className='flex flex-row items-center cursor-pointer' */}
         <SocialIcon className='cursor-pointer' network='email' fgColor='darkGreen' bgColor='transparent'/>
         <p className='uppercase hidden md:inline-flex text-sm text-green-800'>DROP AN EMAIL</p>
       </motion.div>
+      <motion.div
+        className="nav-bg fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+        transition={{duration: 1.5}}
+      ></motion.div>
+      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py0'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-green-900 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+          {
+            links.map(link => {
+              
+              return (
+                <li key={link.hash} className='h-3/4 flex items-center justify-center'>
+                  <Link href={link.hash} className='flex w-full items-center justify-center px-2 py-1 hover:text-yellow-500 transition'>{link.name}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+      
     </header>
   )
 }
