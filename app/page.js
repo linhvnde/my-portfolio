b715be3529b7604e7aa0dc32fa997ea45d621283
linhvnde <linh.nh.tyty@gmail.com>
2023-11-06 21:10:40 +0100

    style: added padding for whole page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import About from '@/components/About';
 import Intro from '@/components/Intro';
-import Navbar from '@/components/Navbar';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main className="h-screen snap-y snap-mandatory overflow-scroll z-0 px-4 ">
       
 
       {/* Intro */}
