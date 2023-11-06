c3236e0627827a98e9f7e9576606bdeb6cd30c1b
linhvnde <linh.nh.tyty@gmail.com>
2023-11-06 20:41:35 +0100

    Feat: Intro w intro text and h1

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -7,25 +7,47 @@ import Image from 'next/image'
 export default function Intro() {
   return (
     <section id='intro' className=' snap-start h-screen flex flex-col items-center justify-start text-center overflow-hidden'>
-      <BackgroundCircle />
-      <motion.div
-        initial={{
-        opacity: 0,
+      <div className='relative'>        
+        <BackgroundCircle />
+        <motion.div
+          initial={{
+          opacity: 0,
 
-      }}
-      animate={{
-        scale: [0.5,0.8,1,1.2,1],
-        opacity: 1,
-        // borderRadius: ["20%", "20%", "50%", "80%", "20%"],
-      }}
-      transition={{
-        duration: 4.5,
-      }}
-        className='relative flex justify-center items-center '>        
-        <div className='profile-image absolute mt-[82vh] overflow-hidden rounded-full border border-emerald-700 h-[20vmin] w-[20vmin]'>        
-          <Image  src="https://res.cloudinary.com/dkuwzoiqf/image/upload/t_Square Profile Picture/v1699225827/my-portfolio/IMG_4909-EDIT_cla1wa.jpg" alt="Linh profile picture" layout='fill' objectFit='cover' quality="100" priority={true} />
+        }}
+        animate={{
+          scale: [0.5,0.8,1,1.2,1],
+          opacity: 1,
+          // borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+        }}
+        transition={{
+          duration: 4.5,
+        }}
+          className='flex justify-center items-center '>        
+          <div className='profile-image absolute mt-[82vh] overflow-hidden rounded-full border border-emerald-700 h-[20vmin] w-[20vmin]'>        
+            <Image  src="https://res.cloudinary.com/dkuwzoiqf/image/upload/t_Square Profile Picture/v1699225827/my-portfolio/IMG_4909-EDIT_cla1wa.jpg" alt="Linh profile picture" layout='fill' objectFit='cover' quality="100" priority={true} />
+          </div>
+        </motion.div>
+        <div>
+
+        <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 5
+            }}
+            >
+            <h1 className='text-lg mt-[54vh]  text-green-900 sm:text-xl uppercase font-semibold tracking-[10px] sm:tracking-[15px]' >
+            Full Stack Developer 
+            </h1>         
+         
+            <p className="max-w-4xl mt-[5vh] px-4 text-base font-medium !leading-[2] sm:text-lg text-green-900 text-justify">              
+              <span className="font-bold">Linh is my name</span> - a <span className="italic">career changer</span> who pivoted to <span className="font-bold">Full Stack Development</span> from a decade as a <span className="italic">Business Strategist</span>. Specialize in the <span className="underline">MERN stack</span> and passionate about crafting <span className="font-bold">innovative software solutions</span> that are both user-intuitive and business-optimal.
+            </p>
+          
+        </motion.div>
         </div>
-      </motion.div>
+      </div>
+       
     </section>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,12 +12,13 @@ export default function Navbar() {
   return (
     <nav>
       <motion.div
-        className="nav-bg fixed top-[5rem] left-1/2  h-[4.5rem] w-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] md:top-6 md:h-[3.25rem] md:w-[34rem] sm:w-[85%] rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="z-[999] nav-bg fixed top-[5rem] left-1/2  h-[4.5rem] w-full border border-white border-opacity-40 bg-white bg-opacity-70 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] md:top-6 md:h-[3.25rem] md:w-[34rem] sm:w-[85%] rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-65
+        "
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         transition={{duration: 1.5}}
       ></motion.div>
-      <motion.nav className='nav-content flex fixed top-[5.15rem] left-1/2 h-12 -translate-x-1/2 py-2 md:top-[1.7rem] md:h-[initial] md:py-2'
+      <motion.nav className='z-[999] nav-content flex fixed top-[5.15rem] left-1/2 h-12 -translate-x-1/2 py-2 md:top-[1.7rem] md:h-[initial] md:py-2'
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         transition={{duration: 1.5}}
@@ -28,7 +29,7 @@ export default function Navbar() {
               
               return (
                 <li key={link.hash} className='h-3/4 flex items-center justify-center'>
-                  <Link href={link.hash} className='flex w-full items-center justify-center px-2 py-1 hover:text-yellow-500 transition'>{link.name}</Link>
+                  <Link href={link.hash} className='flex w-full items-center justify-center px-2 py-1 hover:text-orange-500 transition'>{link.name}</Link>
                 </li>
               )
             })
