0cc97030eb374c0397f5bdeb9dd56261eb1637dc
linhvnde <linh.nh.tyty@gmail.com>
2023-11-06 17:17:47 +0100

    defact: moved Header and Navbar to layout.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import About from '@/components/About';
-import Header from '@/components/Header';
 import Intro from '@/components/Intro';
 import Navbar from '@/components/Navbar';
 import Image from 'next/image';
@@ -7,9 +6,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="h-screen snap-y snap-mandatory overflow-scroll z-0">
-      {/* Header */}
-      <Header />
-      <Navbar />
+      
+
       {/* Intro */}
       <Intro/>
 
