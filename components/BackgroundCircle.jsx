46f99f6e7378412201b9d2f58f8d19b38c8bb6f8
linhvnde <linh.nh.tyty@gmail.com>
2023-11-06 17:22:55 +0100

    style: relocated BgCircle to align w profile-image

--- components/BackgroundCircle.jsx
+++ components/BackgroundCircle.jsx
@@ -11,20 +11,19 @@ export default function BackgroundCircle() {
 
       }}
       animate={{
-        scale: [1,2,2,3,1],
+        scale: [0.5,0.8,1,1.2,1],
         opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1],
-        // borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
       transition={{
         duration: 4.5,
       }}
-      className='relative flex justify-center items-center'>
-      <div className='absolute border border-emerald-700 border-opacity-[50%] rounded-full h-[20vmin] w-[20vmin] mt-[27vh] md:h-[23vmin] md:w-[23vmin]  md:mt-[15vh] animate-ping'/>
-      <div className='absolute border border-emerald-700 border-opacity-[20%] rounded-full h-[30vmin] w-[30vmin] mt-[27vh] md:h-[35vmin] md:w-[35vmin] md:mt-[15vh]'/>
-      <div className='absolute border border-emerald-700 border-opacity-[20%] rounded-full h-[47vmin] w-[47vmin] mt-[27vh] md:h-[55vmin] md:w-[55vmin] md:mt-[15vh]'/>
-      <div className='absolute border border-orange-600 opacity-20 rounded-full h-[60vmin] w-[60vmin] mt-[27vh] md:h-[70vmin] md:w-[70vmin] md:mt-[15vh]  animate-pulse'/>
-      <div className='absolute border border-emerald-700 border-opacity-[20%] rounded-full h-[70vmin] w-[70vmin] mt-[27vh] md:h-[80vmin] md:w-[80vmin] md:mt-[15vh]'/>
-      <div />
+      className='relative flex justify-center items-center '>
+      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[20vmin] w-[20vmin] md:h-[23vmin] md:w-[23vmin]   animate-ping'/>
+      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[30vmin] w-[30vmin] md:h-[35vmin] md:w-[35vmin] '/>
+      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[47vmin] w-[47vmin] md:h-[55vmin] md:w-[55vmin] '/>
+      <div className='absolute mt-[82vh] border border-orange-600 opacity-20 rounded-full h-[60vmin] w-[60vmin] md:h-[70vmin] md:w-[70vmin]   animate-pulse'/>
+      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[70vmin] w-[70vmin] md:h-[80vmin] md:w-[80vmin] '/>
     </motion.div>
   )
 }
