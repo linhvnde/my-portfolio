1ab151c411db39ccc5c07397c082bf26d14f6fe4
linhvnde <linh.nh.tyty@gmail.com>
2023-11-08 13:59:46 +0100

    style: responsive About h3

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 export default function About() {
   return (
     <section id='about' className='snap-start sm:h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-      <h3 className='absolute top-[10rem] md:top-[5rem] my-2 uppercase tracking-[20px] text-emerald-700 text-2xl text-center'>
+      <h3 className='absolute top-[10rem] sm:top-[5rem] my-2 uppercase tracking-[20px] text-emerald-700 text-2xl text-center'>
         About
       </h3>
 
@@ -26,7 +26,7 @@ export default function About() {
         viewport={{ once: true }}
         className='flex justify-center -mt-56'
       >        
-        <div className='about-image relative sm:mt-[27rem] mt-[28rem] md:mt-[12rem] h-56 w-56 sm:h-[30vh] sm:w-[40vw] md:h-[30vh] md:w-[30vw] rounded-lg overflow-hidden'>          
+        <div className='about-image relative sm:mt-[27rem] mt-[28rem] md:mt-[12rem] h-56 w-56 sm:h-[40vh] sm:w-[40vw] md:h-[30vh] md:w-[30vw] rounded-lg overflow-hidden'>          
           <Image
             src="https://res.cloudinary.com/dkuwzoiqf/image/upload/v1699304237/my-portfolio/z4295401506805_9198ca46dcee5224ffc35eafe5eaca58_w9ww9a.jpg"
             alt="Picture about Linh" layout='fill' objectFit='cover' quality="100" priority={true} 
