e5b44e38e8fb15f45e239c6050e8d258d2886409
linhvnde <linh.nh.tyty@gmail.com>
2023-11-13 23:51:09 +0100

    style: responsive BgCir

--- components/BackgroundCircle.jsx
+++ components/BackgroundCircle.jsx
@@ -19,11 +19,11 @@ export default function BackgroundCircle() {
         duration: 4.5,
       }}
       className='relative flex justify-center items-center '>
-      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[20vmin] w-[20vmin] md:h-[23vmin] md:w-[23vmin]   animate-ping'/>
-      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[30vmin] w-[30vmin] md:h-[35vmin] md:w-[35vmin] '/>
-      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[47vmin] w-[47vmin] md:h-[55vmin] md:w-[55vmin] '/>
-      <div className='absolute mt-[82vh] border border-orange-600 opacity-20 rounded-full h-[60vmin] w-[60vmin] md:h-[70vmin] md:w-[70vmin]   animate-pulse'/>
-      <div className='absolute mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[70vmin] w-[70vmin] md:h-[80vmin] md:w-[80vmin] '/>
+      <div className='absolute mt-[90vh] sm:mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[20vmin] w-[20vmin] md:h-[23vmin] md:w-[23vmin]   animate-ping'/>
+      <div className='absolute mt-[90vh] sm:mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[30vmin] w-[30vmin] md:h-[35vmin] md:w-[35vmin] '/>
+      <div className='absolute mt-[90vh] sm:mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[47vmin] w-[47vmin] md:h-[55vmin] md:w-[55vmin] '/>
+      <div className='absolute mt-[90vh] sm:mt-[82vh] border border-orange-600 opacity-20 rounded-full h-[60vmin] w-[60vmin] md:h-[70vmin] md:w-[70vmin]   animate-pulse'/>
+      <div className='absolute mt-[90vh] sm:mt-[82vh] border border-emerald-700 border-opacity-[20%] rounded-full h-[70vmin] w-[70vmin] md:h-[80vmin] md:w-[80vmin] '/>
     </motion.div>
   )
 }
