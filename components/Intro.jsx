4bcf88895bb84e2bdd1a264a8d452892811ce8f2
linhvnde <linh.nh.tyty@gmail.com>
2023-11-13 23:20:57 +0100

    Style: Responsive Navbar w less y-gap

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 export default function Intro() {
   return (
-    <section id='home' className=' snap-start h-screen flex flex-col items-center justify-start text-center overflow-hidden pb-[5vh]'>
+    <section id='home' className=' border border-blue-600 scroll-pt-14 snap-start h-screen flex flex-col items-center justify-start text-center overflow-hidden pb-[5vh]'>
       <div className='relative'>        
         <BackgroundCircle />
         <motion.div
@@ -23,7 +23,7 @@ export default function Intro() {
           duration: 4.5,
         }}
           className='flex justify-center items-center  '>        
-          <div className='profile-image absolute mt-[82vh] overflow-hidden rounded-full h-[20vmin] w-[20vmin]'>        
+          <div className='profile-image absolute mt-[90vh] sm:mt-[82vh] overflow-hidden rounded-full h-[20vmin] w-[20vmin]'>        
             <Image  src="https://res.cloudinary.com/dkuwzoiqf/image/upload/t_Square Profile Picture/v1699225827/my-portfolio/IMG_4909-EDIT_cla1wa.jpg" alt="Linh profile picture" layout='fill' objectFit='cover' quality="100" priority={true} />
           </div>
         </motion.div>
@@ -36,11 +36,11 @@ export default function Intro() {
               duration: 5
             }}
             >
-            <h1 className='text-lg mt-[52vh]  text-emerald-700 sm:text-xl uppercase font-semibold tracking-[10px] sm:tracking-[15px]' >
+            <h1 className='text-lg mt-[58vh] sm:mt-[60vh]  text-emerald-700 uppercase font-semibold tracking-[10px] sm:tracking-[15px]' >
             Full Stack Developer 
             </h1>         
          
-            <p className="max-w-4xl mt-[5vh] px-4 text-base font-medium !leading-[2] sm:text-lg text-emerald-700 text-justify">              
+            <p className="max-w-4xl mt-[1.5vh] sm:mt-[2vh] px-4 text-sm font-medium !leading-[2] sm:text-base text-emerald-700 text-justify">              
               <span className="font-bold">Linh is my name</span> - a <span className="italic">career changer</span> who pivoted to <span className="font-bold">Full Stack Development</span> from a decade as a <span className="italic">Business Strategist</span>. Specialize in the <span className="underline">MERN stack</span> and passionate about crafting <span className="font-bold">innovative software solutions</span> that are both user-intuitive and business-optimal.
             </p>
           
