4572acc5bd39e1dd28d1455c01ead37ca5c090bc
linhvnde <linh.nh.tyty@gmail.com>
2023-11-13 14:41:21 +0100

    Style: responsive w container for CV dl Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className='sticky top-0 p-3 flex items-start justify-center md:justify-between max-w-7xl mx-auto xl:items-center
     z-[999]'>
-      <motion.div className='flex w-[10rem] flex-row mt-1.5 justify-evenly items-center header-1'
+      <motion.div className='flex w-[10rem] flex-row mt-[0.75rem] justify-evenly items-center header-1'
         initial={{ x: -500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{
@@ -25,16 +25,16 @@ export default function Header() {
         <SocialIcon className=' focus:scale-110 hover:scale-110' url="https://www.linkedin.com/in/linhnhvn-fullstackdev/" fgColor='darkGreen' bgColor='transparent'/>
       </motion.div>
       <motion.div
-        className='flex w-[12rem] flex-row mt-1.5 justify-evenly items-center cursor-pointer header-1'
+        className='flex w-[10rem] flex-row mt-[0.75rem] justify-evenly items-center cursor-pointer header-1'
         initial={{ x: 500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{
           duration: 1.5
         }}        
       >
-        <a href='/CV - Linh Nguyen Hoang l Full Stack Developer.pdf' download title='Download My CV' className=' focus:scale-110 hover:scale-110 text-green-800'><HiDownload/> </a>
+        <a href='/CV - Linh Nguyen Hoang l Full Stack Developer.pdf' download title='Download My CV' className='h-[50px] w-[50px] flex justify-center items-center focus:scale-110 hover:scale-110 text-green-800'><HiDownload/> </a>
         <SocialIcon className='focus:scale-110 hover:scale-110 cursor-pointer' network='email' url="mailto:linh.nh.tyty@gmail.com" fgColor='darkGreen' bgColor='transparent'/>
-        <p className='uppercase md:inline-flex text-sm text-green-800'><a href="mailto:linh.nh.tyty@gmail.com"> DROP AN EMAIL</a></p>
+        {/* <p className='uppercase md:inline-flex text-sm text-green-800'><a href="mailto:linh.nh.tyty@gmail.com"> EMAIL ME</a></p> */}
       </motion.div>
     
       
