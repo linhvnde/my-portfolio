3c43ecbcbeb5ebc623150336d1fca25873bddb2e
linhvnde <linh.nh.tyty@gmail.com>
2023-11-15 03:29:12 +0100

    style: Projects & ProjectCard w horizontal scroll

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,29 +1,80 @@
+"use client"
+
+import { motion } from 'framer-motion'
+
 import Image from 'next/image'
 import React from 'react'
 
 export default function ProjectCard(props) {
   return (
-    <article className='relative overflow-hidden mb-3 px-0 md:px-10 w-[80vw] h-[50vh] border rounded-lg border-emerald-500/5 bg-emerald-50/80'>
-      <div className='mt-6 pt-4 pb-10 px-1 sm:pl-2 sm:pr-1 max-w-[55%] flex flex-col h-full'>
-
-      <h3 className='text-2xl font-semibold text-emerald-800'>{props.title}</h3>
-      <p className='mt-2 leading-relaxed text-emerald-700/80'>{props.description}</p>
-      <ul className='flex flex-wrap mb-8 mt-4 sm:mt-auto pb-2 gap-2'>{props.tags.map(tag => (
-        <li className=' bg-emerald-700/[0.7] rounded-full px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white'>{ tag }</li>
-      ))}</ul>
-
-      </div>
-
-      <div className='absolute top-[2.8rem] -right-10 w-[25rem] h-[15rem] rounded-lg overflow-hidden shadow-2xl'>
+    <article className='project-card snap-center overflow-hidden relative top-[10vh] p-2  border border-red-500 flex-shrink-0 w-[90vw] pt-1 flex flex-col justify-evenly items-center  sm:w-[80vw] sm:h-[65vh] rounded-lg sm:border-emerald-500/5 sm:bg-emerald-50/80 shadow-2xl'>      
+    <motion.h3
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration:1
+        }}
+        whileInView={{
+          x: 0,
+          opacity:1,
+        }}
+        viewport={{ once: true }}
+        className='self-start sm:absolute  sm:top-4  sm:left-2 text-base uppercase text-emerald-500'
+      > <a href={`${props.appUrl}`} title='Go to application site' target='blank'>
+          
+        {props.title}
+      </a>
+      </motion.h3>
+      <motion.div
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration:1
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+          
+        }}
+        viewport={{ once: true }}
+        className='sm:absolute sm:top-[2.8rem] sm:-right-[11rem] md:-right-[14rem] sm:mb-3 sm:px-0 md:px-10 border border-purple-500 project-image relative w-[90vw] h-[30.3vh] sm:h-[34vh] sm:w-[80vw] md:h-[40vh] rounded-lg overflow-hidden sm:shadow-2xl'
+      >
         <Image
-          src={props.imageUrl}
-          alt="Snapshot of the application i worked on"
-          layout='fill'
-          style={{ objectFit: 'start' }}
-          quality="100"
-          priority={true}
-        />
-      </div>
+            src={props.imageUrl}
+            alt="Snapshot of the application i worked on" layout='fill' objectFit='contain' quality="100" priority={true} 
+          />
+      </motion.div>
+        {/* <div >          
+         
+        </div> */}
+      <motion.div
+        className='project-text sm:absolute sm:top-7 md:top-9 sm:self-start sm:pl-3 sm:pr-10 sm:max-w-[55%] lg:max-w-[50%] sm:h-[50vh] relative mt-[2vh] sm:mt-[3vh] md:mt-[1.8vh] text-xs sm:text-sm md:text-base space-y-3 sm:space-y-3 md:space-y-7 px-0 md:pl-5 text-justify text-emerald-700  flex flex-col sm:justify-evenly items-center'
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration:1
+        }}
+        whileInView={{
+          x: 0,
+          opacity:1,
+        }}
+        viewport={{ once: true }}      
+      >
+        <p className='leading-relaxed text-emerald-700/80'>{props.description}</p>
+        <ul className='flex flex-wrap self-end mb-1 mt-1 sm:mt-auto pb-1 gap-1 pr-2'>
+          {props.tags.map(tag => (
+            <li className=' bg-emerald-700/[0.7] rounded-full px-1 py-1 text-[0.6rem] uppercase tracking-wide text-white'>
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </motion.div>
     </article>
   )
 }
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,15 +1,20 @@
+"use client"
+
+import { motion } from 'framer-motion'
+
 import React from 'react'
 import SectionHeading from './section-heading'
 import ProjectCard from './ProjectCard'
 import { projectsData } from '@/lib/data'
 
+
 export default function Projects() {
   return (
-    <section id='projects' className=' h-screen snap-start flex flex-col relative text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center overflow-hidden '>
+    <section id='projects' className='border border-blue-600 snap-start h-screen relative flex flex-col justify-evenly items-center  scroll-mt-16  mx-0 sm:mx-auto w-full sm:max-w-full px-0 sm:px-10'>
       <SectionHeading>
         Projects
       </SectionHeading>
-      <div className='mt-16 max-h-[65vh]'>
+      <div className='w-full flex space-x-[5vw] px-[2.5vw] sm:space-x-[15vw] overflow-x-scroll p-0 sm:p-10 snap-x snap-mandatory'>
        {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <ProjectCard {...project} />
